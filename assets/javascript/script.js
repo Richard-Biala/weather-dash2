632cb8cb3700af0a559b7167295cec7dcf52bc52
Richard <rkj128@gmail.com>
2023-12-28 14:34:14 -0800

    initial card for today's weather

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -15,12 +15,26 @@ $(document).ready(function(){
     //function that searches for today's weather
     function searchWeather (cityName){
 
+        //clear out the today div
+        $("#today").empty();
+
         //quesry the API
         $.ajax({
             type: "GET",
             url: `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=53d2378036e10278637205320c39dd84`
         }).then(function(response){
             console.log(response);
+
+            const name = response.name;
+
+            console.log(name);
+
+
+            //create card title
+            const title = $("<h3>").addClass("card-title").text(`${name} (${new Date().toLocaleDateString()})`);
+
+
+            $("#today").append(title);
         })
     }
 
