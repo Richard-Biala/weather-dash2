b7f3334834e0050346d99e081e2a0ebb89d43d74
Richard <rkj128@gmail.com>
2023-12-30 16:18:24 -0800

    changed temp value to read fahrenheit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -30,7 +30,7 @@ $(document).ready(function(){
         //quesry the API
         $.ajax({
             type: "GET",
-            url: `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=53d2378036e10278637205320c39dd84`
+            url: `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=53d2378036e10278637205320c39dd84&units=imperial`
         }).then(function(response){
                 console.log(response);
 
@@ -122,7 +122,7 @@ $(document).ready(function(){
     function getForecast(cityName){
         $.ajax({
             type: "GET",
-            url: `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=53d2378036e10278637205320c39dd84`
+            url: `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=53d2378036e10278637205320c39dd84&units=imperial`
         }).then(function(response){
 
                 console.log(response);
